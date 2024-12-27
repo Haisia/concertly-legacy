6b29086ee1ae25f9ec0eb0cae81def863d4da1bc
최준혁 <cwnsgur13579@gmail.com>
2024-12-27 16:26:16 +0900

    스키마 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,7 +5,7 @@ create table if not exists users
   password    varchar(255) not null comment '패스워드',
   point       bigint           not null comment '보유 포인트',
 
-  deleted     varchar(1) not null default 'N' check (deleted in ('N', 'Y')),
+  deleted     varchar(1) not null default 'N',
   created_at  datetime(6) not null default now(),
   created_by  varchar(255) null,
   updated_at  datetime(6) null,
@@ -21,7 +21,7 @@ create table if not exists concerts
   start_time  datetime(6)    not null comment '콘서트 시작 일시',
   end_time    datetime(6)    not null comment '콘서트 종료 일시',
 
-  deleted     varchar(1) not null default 'N' check (deleted in ('N', 'Y')),
+  deleted     varchar(1) not null default 'N',
   created_at  datetime(6) not null default now(),
   created_by  varchar(255) null,
   updated_at  datetime(6) null,
@@ -33,9 +33,9 @@ create table if not exists seats
   id            int           not null auto_increment comment 'pk' primary key,
   concert_id    int           not null comment '콘서트 pk',
   seat_number   varchar(255)  not null comment '좌석 번호 ex) a1, a2, ... , e1, e2, ...',
-  status        varchar(255)   not null default 'AVAILABLE' check (status in ('AVAILABLE', 'RESERVED')) comment '좌석 상태 ex) AVAILABLE, RESERVED',
+  status        enum('AVAILABLE', 'RESERVED')   not null default 'AVAILABLE' comment '좌석 상태 ex) AVAILABLE, RESERVED',
 
-  deleted     varchar(1) not null default 'N' check (deleted in ('N', 'Y')),
+  deleted     varchar(1) not null default 'N',
   created_at  datetime(6) not null default now(),
   created_by  varchar(255) null,
   updated_at  datetime(6) null,
