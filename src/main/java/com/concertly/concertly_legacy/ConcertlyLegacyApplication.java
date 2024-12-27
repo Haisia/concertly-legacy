0a3f4dcc97e1914dec2a1507aaa06806ad318ae5
최준혁 <cwnsgur13579@5finger.co.kr>
2024-12-27 12:56:14 +0900

    init

---
+++ src/main/java/com/concertly/concertly_legacy/ConcertlyLegacyApplication.java
@@ -0,0 +1,13 @@
+package com.concertly.concertly_legacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcertlyLegacyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcertlyLegacyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/concertly/concertly_legacy/ConcertlyLegacyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.concertly.concertly_legacy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcertlyLegacyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
