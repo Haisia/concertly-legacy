7f6b79659f3f1348d2eec8cbc3a0a8f191c7a454
최준혁 <cwnsgur13579@5finger.co.kr>
2024-12-27 13:19:45 +0900

    BaseEntity 와 AuditAwareImpl 을 추가하여 Auditing 구현.

---
+++ src/main/java/com/concertly/concertly_legacy/commons/entity/BaseEntity.java
@@ -0,0 +1,44 @@
+package com.concertly.concertly_legacy.commons.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter @Setter
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public class BaseEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
+  private Long id;
+
+  // 'Y' or 'N'
+  @Column
+  protected String deleted = "N";
+
+  @CreatedDate
+  @Column(updatable = false)
+  protected LocalDateTime createdAt;
+
+  @CreatedBy
+  @Column(updatable = false)
+  protected String createdBy;
+
+  @LastModifiedDate
+  @Column(insertable = false)
+  protected LocalDateTime updatedAt;
+
+  @LastModifiedBy
+  @Column(insertable = false)
+  protected String updatedBy;
+
+}
