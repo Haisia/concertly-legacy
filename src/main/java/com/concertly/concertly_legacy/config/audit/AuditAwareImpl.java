a30d9c907c5f2640a6bf6249184b9ca8871e97fa
Haisia <cwnsgur13579@gmail.com>
2024-12-28 15:02:56 +0900

    createdBy 오디팅 설정

--- src/main/java/com/concertly/concertly_legacy/config/audit/AuditAwareImpl.java
+++ src/main/java/com/concertly/concertly_legacy/config/audit/AuditAwareImpl.java
@@ -1,6 +1,10 @@
 package com.concertly.concertly_legacy.config.audit;
 
+import com.concertly.concertly_legacy.config.jwt.ConcertlyUserDetail;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
@@ -10,7 +14,14 @@ public class AuditAwareImpl implements AuditorAware<String> {
 
   @Override
   public Optional<String> getCurrentAuditor() {
-    return Optional.of( "AUDIT");
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    if (authentication == null || !authentication.isAuthenticated() ||
+      !(authentication.getPrincipal() instanceof UserDetails)) {
+      return Optional.of( "AUDIT");
+    } else {
+      ConcertlyUserDetail userDetails = (ConcertlyUserDetail) authentication.getPrincipal();
+      return Optional.of(userDetails.getUser().getId().toString());
+    }
   }
 
 }
