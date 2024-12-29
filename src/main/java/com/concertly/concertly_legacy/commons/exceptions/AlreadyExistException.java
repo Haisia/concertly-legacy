3b5d261507f8e60ebfc0f22d02c755a23bff9f10
Haisia <cwnsgur13579@gmail.com>
2024-12-29 15:52:29 +0900

    AlreadyExistException 메세지 파라미터 수정

--- src/main/java/com/concertly/concertly_legacy/commons/exceptions/AlreadyExistException.java
+++ src/main/java/com/concertly/concertly_legacy/commons/exceptions/AlreadyExistException.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.CONFLICT)
 public class AlreadyExistException extends RuntimeException {
-  public AlreadyExistException(String existValue) {
-    super(String.format("%s 는 이미 존재하는 값 입니다.", existValue));
+  public AlreadyExistException(String message) {
+    super(message);
   }
 }
