4bf7a7c643457ada9b8145352e55b790f5478286
Haisia <cwnsgur13579@gmail.com>
2025-01-01 00:07:38 +0900

    빌더클래스 예외처리

--- src/main/java/com/concertly/concertly_legacy/domain/concert/dto/BaseConcertDtoBuilder.java
+++ src/main/java/com/concertly/concertly_legacy/domain/concert/dto/BaseConcertDtoBuilder.java
@@ -1,8 +1,12 @@
 package com.concertly.concertly_legacy.domain.concert.dto;
 
 import com.concertly.concertly_legacy.domain.concert.entity.Concert;
+import com.concertly.concertly_legacy.domain.concert.entity.ConcertComment;
+import com.concertly.concertly_legacy.domain.concert.entity.Seat;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
 
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class BaseConcertDtoBuilder {
@@ -10,16 +14,21 @@ public class BaseConcertDtoBuilder {
   private BaseConcertDto target;
 
   public BaseConcertDtoBuilder withSeats() {
-    target.setSeatList(BaseSeatDto.from(concert.findAvailableSeatList()));
+    List<Seat> availableSeatList = concert.findAvailableSeatList();
+    if (availableSeatList == null) throw new IllegalArgumentException("seatList 를 찾을 수 없습니다.");
+    target.setSeatList(BaseSeatDto.from(availableSeatList));
     return this;
   }
 
   public BaseConcertDtoBuilder withComments() {
-    target.setCommentList(BaseConcertCommentDto.from(concert.getCommentList()));
+    List<ConcertComment> commentList = concert.getCommentList();
+    if (commentList == null) throw new IllegalArgumentException("commentList 를 찾을 수 없습니다.");
+    target.setCommentList(BaseConcertCommentDto.from(commentList));
     return this;
   }
 
   public static BaseConcertDtoBuilder builder(Concert concert) {
+    if (concert == null) throw new IllegalArgumentException("concert 은 null 이면 안됩니다.");
     BaseConcertDtoBuilder builder = new BaseConcertDtoBuilder(concert);
     builder.target = BaseConcertDto.from(concert);
     return builder;
--- src/main/java/com/concertly/concertly_legacy/domain/reservation/dto/BaseReservationDtoBuilder.java
+++ src/main/java/com/concertly/concertly_legacy/domain/reservation/dto/BaseReservationDtoBuilder.java
@@ -2,8 +2,10 @@ package com.concertly.concertly_legacy.domain.reservation.dto;
 
 import com.concertly.concertly_legacy.domain.concert.dto.BaseConcertDto;
 import com.concertly.concertly_legacy.domain.concert.dto.BaseSeatDto;
+import com.concertly.concertly_legacy.domain.concert.entity.Seat;
 import com.concertly.concertly_legacy.domain.reservation.entity.Reservation;
 import com.concertly.concertly_legacy.domain.user.dto.BaseUserDto;
+import com.concertly.concertly_legacy.domain.user.entity.User;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 
@@ -13,21 +15,29 @@ public class BaseReservationDtoBuilder {
   private BaseReservationDto target;
 
   public BaseReservationDtoBuilder withConcert() {
-    target.setConcert(BaseConcertDto.from(reservation.getSeat().getConcert()));
+    Seat seat = reservation.getSeat();
+    if (seat == null) throw new IllegalArgumentException("seat 를 찾을 수 없습니다.");
+    if (seat.getConcert() == null) throw new IllegalArgumentException("concert 를 찾을 수 없습니다.");
+    target.setConcert(BaseConcertDto.from(seat.getConcert()));
     return this;
   }
 
   public BaseReservationDtoBuilder withSeat() {
-    target.setSeat(BaseSeatDto.from(reservation.getSeat()));
+    Seat seat = reservation.getSeat();
+    if (seat == null) throw new IllegalArgumentException("seat 를 찾을 수 없습니다.");
+    target.setSeat(BaseSeatDto.from(seat));
     return this;
   }
 
   public BaseReservationDtoBuilder withUser() {
-    target.setUser(BaseUserDto.from(reservation.getUser()));
+    User user = reservation.getUser();
+    if (user == null) throw new IllegalArgumentException("user 를 찾을 수 없습니다.");
+    target.setUser(BaseUserDto.from(user));
     return this;
   }
 
   public static BaseReservationDtoBuilder builder(Reservation reservation) {
+    if (reservation == null) throw new IllegalArgumentException("Reservation 은 null 이면 안됩니다.");
     BaseReservationDtoBuilder builder = new BaseReservationDtoBuilder(reservation);
     builder.target = BaseReservationDto.from(reservation);
     return builder;
